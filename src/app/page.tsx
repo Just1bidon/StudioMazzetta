cf9f883995bccb8a949150467ce97ae511674ef4
baptistegoncalves <baptiste.goncalves@epitech.digital>
2024-08-16 10:00:47 +0200

    ça marche passsss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
+import NavBar from "@/component/Navbar";
 
 export default function Home() {
   return (
-    <main>
+    <main className="bg-[#8e9cc7] h-32">
+      <NavBar></NavBar>
     </main>
   );
 }
---
+++ src/component/Navbar.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function NavBar() {
+  return (
+    <section className="bg-green-600 w-full h-[83px]">
+      <p>NavBar</p>
+    </section>
+  );
+}
