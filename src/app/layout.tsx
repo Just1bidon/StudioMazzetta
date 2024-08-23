2996b03699d745786cefadf67670c6982e1fc20e
baptistegoncalves <baptiste.goncalves@epitech.digital>
2024-08-23 12:46:51 +0200

    test nouvelles options pour les fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,46 @@
-'use client'
+"use client";
 
 import "@/ui/globals.css";
 import NavBar from "./component/Navbar";
-import { amiko, amita } from "@/app/fonts";
 import Footer from "./component/Footer";
+import localFont from "next/font/local";
+
+export const amita = localFont({
+  src: [
+    {
+      path: "../../public/fonts/Amita/Amita-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Amita/Amita-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+  variable: "--font-amita",
+});
+
+export const amiko = localFont({
+  src: [
+    {
+      path: "../../public/fonts/amiko/Amiko-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/amiko/Amiko-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/amiko/Amiko-SemiBold.ttf",
+      weight: "600",
+      style: "normal",
+    },
+  ],
+  variable: "--font-amiko",
+});
 
 export default function RootLayout({
   children,
