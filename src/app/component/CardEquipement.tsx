7a3598936f49ec4a67ebcd8afae9b70ada5508c4
baptistegoncalves <baptiste.goncalves@epitech.digital>
2024-09-08 12:14:15 +0200

    icon bleu

--- src/app/component/CardEquipement.tsx
+++ src/app/component/CardEquipement.tsx
@@ -15,9 +15,9 @@ export default function EquipmentCard({
 }: EquipmentCardProps) {
   return (
     <div className="w-[220px] h-[170px] bg-[#FFFFFF] shadow-lg rounded-lg py-4 px-6 flex flex-col justify-start">
-      <div className="flex justify-between items-center">
+      <div className="flex justify-between items-start">
         <h2 className="text-[#000000] text-lg font-bold">{title}</h2>
-        <Image src={iconUrl} alt={`${title}-icon`} width={30} height={30} className="text-[#243662]" />
+        <Image src={iconUrl} alt={`${title}-icon`} width={30} height={30} />
       </div>
       <p className="text-[#000000] mt-4 text-sm text-justify">{description}</p>
       {subDescription && (
