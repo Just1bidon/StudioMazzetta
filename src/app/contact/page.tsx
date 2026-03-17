cb45c6659e71e5ba8bbb0e17d9366a1b16b223ec
baptistegoncalves <baptiste.goncalves@epitech.digital>
2026-03-17 11:31:22 +0100

    DEL formulaire de contact UPD "Nous contacter" ->"Me contacter"

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -16,7 +16,7 @@ export default function ContactPage() {
       {/* Fond blanc incliné */}
       <div className="absolute -z-5 bg-[#F8F9FC] w-[50px] lg:w-[200px] h-[1500px] lg:h-screen top-0 rotate-90 lg:rotate-3 left-[33%] azerty:left-[38%] -translate-y-44 lg:translate-y-0 transition-all duration-500 ease-in-out"></div>
 
-      <div className="relative flex flex-col items-center p-16">
+      <div className="relative flex flex-col items-center px-7 pt-28 sm:p-16">
         <div className="w-full lg:w-[1225px] max-w-6xl lg:flex justify-center items-center gap-[40px] azerty:gap-[200px] transition-all duration-500 ease-in-out">
           {/* Informations de contact */}
           <div className="flex flex-col items-start space-y-6 text-[#243662] w-full lg:w-1/2 transition-all duration-500 ease-in-out">
@@ -24,7 +24,7 @@ export default function ContactPage() {
               <h1
                 className={`${amita.className} text-4xl text-black mb-4 transition-all duration-500 ease-in-out`}
               >
-                Nous contacter
+                Me contacter
               </h1>
               <p className="text-lg text-[#243662] italic -mt-4 transition-all duration-500 ease-in-out">
                 Amélie
@@ -32,10 +32,10 @@ export default function ContactPage() {
             </div>
             <div>
               <p className="text-black transition-all duration-500 ease-in-out">
-                Nous sommes là pour répondre à toutes vos questions.
+                Je suis là pour répondre à toutes vos questions.
               </p>
               <p className="text-black transition-all duration-500 ease-in-out">
-                N’hésitez pas à nous contacter !
+                N’hésitez pas à me contacter !
               </p>
             </div>
 
@@ -109,66 +109,11 @@ export default function ContactPage() {
             </div>
           </div>
 
-          {/* Formulaire de contact */}
-          <div className="relative bg-white shadow-lg rounded-lg p-8 w-full lg:w-1/3 mt-40 lg:mt-0 transition-all duration-500 ease-in-out">
-            {/* Image de fond visible en dessous de 1024px */}
-            <div className="lg:hidden absolute top-[-50px] bottom-[-50px] left-1/2 transform -translate-x-1/2 -z-10 bg-[#F8F9FC] w-screen h-[calc(100%+100px)] transition-all duration-500 ease-in-out">
-              <Image
-                src="/IMG_landscape/Porto-Vecchio/PortoVecchio10.JPG"
-                alt="Fond de contact"
-                layout="fill"
-                objectFit="cover"
-                className="filter brightness-50 blur-sm scale-110 transition-all duration-500 ease-in-out"
-              />
-            </div>
-            <form>
-              <div className="mb-4">
-                <label className="block text-black text-sm font-bold mb-2 transition-all duration-500 ease-in-out">
-                  Name
-                </label>
-                <input
-                  type="text"
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline transition-all duration-500 ease-in-out"
-                  placeholder="Votre nom"
-                />
-              </div>
-              <div className="mb-4">
-                <label className="block text-black text-sm font-bold mb-2 transition-all duration-500 ease-in-out">
-                  Surname
-                </label>
-                <input
-                  type="text"
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline transition-all duration-500 ease-in-out"
-                  placeholder="Votre prénom"
-                />
-              </div>
-              <div className="mb-4">
-                <label className="block text-black text-sm font-bold mb-2 transition-all duration-500 ease-in-out">
-                  Email
-                </label>
-                <input
-                  type="email"
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline transition-all duration-500 ease-in-out"
-                  placeholder="Votre email"
-                />
-              </div>
-              <div className="mb-4">
-                <label className="block text-black text-sm font-bold mb-2 transition-all duration-500 ease-in-out">
-                  Message
-                </label>
-                <textarea
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline transition-all duration-500 ease-in-out"
-                  rows={4}
-                  placeholder="Votre message"
-                />
-              </div>
-              <button
-                type="submit"
-                className="bg-[#243662] text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline hover:bg-blue-800 transition-all duration-500 ease-in-out"
-              >
-                Envoyer
-              </button>
-            </form>
+          {/* Rectangle placeholder pour remplacer le formulaire de contact pour conserver la structure */}
+          <div
+            aria-hidden="true"
+            className="relative hidden md:block h-[560px] w-full lg:w-1/3 mt-40 lg:mt-0"
+          >
           </div>
         </div>
       </div>
